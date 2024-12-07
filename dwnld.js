f9babf9252a35a6c740f1ce401d6febaf0a11ea0
Yazir <alexishernandezgt05@gmail.com>
2024-12-07 17:44:59 -0600

    tercer medio  envio

--- dwnld.js
+++ dwnld.js
@@ -1,6 +1,6 @@
 function descargarAPK() {
     const enlace = document.createElement('a'); // Crear un elemento de enlace
-    enlace.href = './deportes.apk'; // Ruta del archivo
-    enlace.download = 'deportes.apk'; // Nombre del archivo para la descarga
+    enlace.href = './Deportes.apk'; // Ruta del archivo
+    enlace.download = 'Deportes.apk'; // Nombre del archivo para la descarga
     enlace.click(); // Simular clic para iniciar la descarga
 }
